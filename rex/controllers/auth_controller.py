dcbf16e67c0f0a04e4d29e005f9d756edf4f9f17
Carlo Zamagni <carlozamagni@gmail.com>
2013-07-23 22:27:28 +0200

    authorization skeleton

---
+++ rex/controllers/auth_controller.py
@@ -0,0 +1,25 @@
+from flask import Blueprint, request, session, redirect, url_for, render_template
+from rex import app, db
+
+__author__ = 'carlozamagni'
+
+auth = Blueprint('auth', __name__, static_folder='static', template_folder='templates')
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            return redirect(url_for('main_page'))
+    return render_template('login.html', error=error)
+
+
+@auth.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    return redirect(url_for('main_page'))
