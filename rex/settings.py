f59ef027b9c424075a688172503f5dfd83834f9a
Carlo Zamagni <carlozamagni@gmail.com>
2013-08-20 14:39:18 +0200

    db configs

--- rex/settings.py
+++ rex/settings.py
@@ -4,7 +4,7 @@ SECRET_KEY = 'this_obviously_should_be_changed_using_os.urandom()'
 
 
 MONGODB_DATABASE = 'rex'
-MONGODB_HOST = 'localhost'
-#MONGODB_PORT = 31948
-#MONGODB_USERNAME = 'rex'
-#MONGODB_PASSWORD = 'rex'
+MONGODB_HOST = 'ds031948.mongolab.com'
+MONGODB_PORT = 31948
+MONGODB_USERNAME = 'rex'
+MONGODB_PASSWORD = 'rex'
