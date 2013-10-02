3f507f9c4c64080331f867f8a8cbdeffeff2ef33
Carlo Zamagni <cazamagni@technogym.com>
2013-10-02 13:25:12 +0200

    login process

--- rex/controllers/auth_controller.py
+++ rex/controllers/auth_controller.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, session, redirect, url_for, render_template
-from flask.ext.login import login_user
+from flask.ext.login import login_user, logout_user
 from rex import app, db
 from rex.models import user_model
 
@@ -34,4 +34,5 @@ def login():
 @auth_ctrl.route('/logout')
 def logout():
     session.pop('logged_in', None)
+    logout_user()
     return redirect(url_for('main_page'))
